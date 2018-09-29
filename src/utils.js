a9517d73eb2dad69fb1055c6b18b6b327a6784f2
Joe McGrath <joe.mcgrath@fanduel.com>
2018-09-29 20:00:35 +0100

    create url from config

--- src/utils.js
+++ src/utils.js
@@ -1,2 +1,25 @@
 export const isStringWithLength = val =>
 	typeof val === 'string' && val.length > 0
+
+export const removeUndefined = obj => {
+	Object.keys(obj).forEach(key => {
+		if (obj[key] === undefined) {
+			delete obj[key]
+		}
+	})
+	return obj
+}
+
+export const createQuery = obj =>
+	Object.entries(removeUndefined(obj)).reduce(
+		(str, [key, val], i) => `${str}${i > 0 ? '&' : ''}${key}=${val}`,
+		'?',
+	)
+
+export const createGetUrl = config => (query = {}) =>
+	`https://api.tumblr.com/v2/blog/${
+		config.username
+	}.tumblr.com/posts${createQuery({
+		api_key: config.key,
+		...query,
+	})}`
