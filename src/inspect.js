33710bbbf3ca4175c38cdda9a34e7ac05112928a
Joe McGrath <joe.mcgrath@fanduel.com>
2018-10-13 23:16:27 +0100

    skip file selection when only one file exists

--- src/inspect.js
+++ src/inspect.js
@@ -3,27 +3,36 @@ import { readdir } from 'fs'
 
 import { inspect, logEmptyLine } from './utils'
 
-readdir('./output', (err, files) => {
-	const jsonFiles = files.filter(file => file.slice(-5) === '.json')
+const selectFile = () =>
+	new Promise(resolve =>
+		readdir('./output', (err, files) => {
+			const jsonFiles = files.filter(file => file.slice(-5) === '.json')
 
-	if (!jsonFiles.length) {
-		console.log('🛑  No json files found in the output directory')
-		process.exit()
-	}
+			if (!jsonFiles.length) {
+				console.log('🛑  No json files found in the output directory')
+				process.exit()
+			}
 
-	inquirer
-		.prompt([
-			{
-				type: 'list',
-				name: 'fileName',
-				message: 'Select a file:',
-				choices: jsonFiles,
-			},
-		])
-		.then(({ fileName }) => {
-			const posts = require(`../output/${fileName}`)
+			return jsonFiles.length > 1
+				? inquirer
+						.prompt([
+							{
+								type: 'list',
+								name: 'fileName',
+								message: 'Select a file:',
+								choices: jsonFiles,
+							},
+						])
+						.then(resolve)
+				: resolve({ fileName: jsonFiles[0] })
+		}),
+	)
 
-			logEmptyLine()
-			console.log(inspect(posts.reduce((a, b) => Object.assign(a, b), {})))
-		})
-})
+const renderOutput = ({ fileName }) => {
+	const posts = require(`../output/${fileName}`)
+
+	logEmptyLine()
+	console.log(inspect(posts.reduce((a, b) => Object.assign(a, b), {})))
+}
+
+selectFile().then(renderOutput)
--- src/listValues.js
+++ src/listValues.js
@@ -3,51 +3,59 @@ import { readdir } from 'fs'
 
 import { logEmptyLine } from './utils'
 
-readdir('./output', (err, files) => {
-	const jsonFiles = files.filter(file => file.slice(-5) === '.json')
+const selectFile = () =>
+	new Promise(resolve =>
+		readdir('./output', (err, files) => {
+			const jsonFiles = files.filter(file => file.slice(-5) === '.json')
 
-	if (!jsonFiles.length) {
-		console.log('🛑  No json files found in the output directory')
-		process.exit()
+			if (!jsonFiles.length) {
+				console.log('🛑  No json files found in the output directory')
+				process.exit()
+			}
+
+			return jsonFiles.length > 1
+				? inquirer
+						.prompt([
+							{
+								type: 'list',
+								name: 'fileName',
+								message: 'Select a file:',
+								choices: jsonFiles,
+							},
+						])
+						.then(resolve)
+				: resolve({ fileName: jsonFiles[0] })
+		}),
+	)
+
+const selectField = ({ fileName }) => {
+	const posts = require(`../output/${fileName}`)
+	const fields = Object.keys(posts.reduce((a, b) => Object.assign(a, b), {}))
+
+	return inquirer.prompt([
+		{
+			type: 'list',
+			name: 'field',
+			message: 'Select a field:',
+			choices: fields,
+		},
+	]).then(res => [posts, res])
+}
+
+const renderOutput = ([posts, { field }]) => {
+	const values = posts.reduce((set, post) => set.add(post[field]), new Set())
+
+	logEmptyLine()
+
+	if (!values.size) {
+		console.log(`No values were found for the field "${field}"`)
 	}
 
-	inquirer
-		.prompt([
-			{
-				type: 'list',
-				name: 'fileName',
-				message: 'Select a file:',
-                choices: jsonFiles,
-			},
-		])
-        .then(({ fileName }) => {
-			const posts = require(`../output/${fileName}`)
-			const fields = Object.keys(posts.reduce((a, b) => Object.assign(a, b), {}))
+	console.log(`Found ${values.size} values for the field "${field}":`)
+	logEmptyLine()
+	console.log(JSON.stringify(values, null, 2))
+}
 
-			inquirer
-				.prompt([
-					{
-						type: 'list',
-						name: 'field',
-						message: 'Select a field:',
-						choices: fields,
-					},
-				])
-				.then(({ field }) => {
-					const values = posts.reduce(
-						(set, post) => set.add(post[field]),
-						new Set(),
-                    )
-
-                    logEmptyLine()
-                    
-                    if (!values.size) {
-                        console.log(`No values were found for the field "${field}"`)
-                    }
-
-                    console.log(`Found ${values.size} values for the field "${field}":`)
-                    logEmptyLine()
-                    console.log(JSON.stringify(values, null, 2))
-				})
-		})
-})
+selectFile()
+	.then(selectField)
+	.then(renderOutput)
