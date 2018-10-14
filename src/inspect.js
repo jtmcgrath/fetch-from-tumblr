18ab7fd36037f3798b2c9d94ff5899226ab92e79
Joe McGrath <joe.mcgrath@fanduel.com>
2018-10-14 12:35:12 +0100

    switch to async/await

--- src/inspect.js
+++ src/inspect.js
@@ -3,36 +3,29 @@ import { readdir } from 'fs'
 
 import { inspect, logEmptyLine } from './utils'
 
-const selectFile = () =>
-	new Promise(resolve =>
-		readdir('./output', (err, files) => {
-			const jsonFiles = files.filter(file => file.slice(-5) === '.json')
+readdir('./output', async (err, files) => {
+	const jsonFiles = files.filter(file => file.slice(-5) === '.json')
 
-			if (!jsonFiles.length) {
-				console.log('🛑  No json files found in the output directory')
-				process.exit()
-			}
+	if (!jsonFiles.length) {
+		console.log('🛑  No json files found in the output directory')
+		process.exit()
+	}
 
-			return jsonFiles.length > 1
-				? inquirer
-						.prompt([
-							{
-								type: 'list',
-								name: 'fileName',
-								message: 'Select a file:',
-								choices: jsonFiles,
-							},
-						])
-						.then(resolve)
-				: resolve({ fileName: jsonFiles[0] })
-		}),
-	)
+	const { fileName } =
+		jsonFiles.length > 1
+			? await inquirer.prompt([
+					{
+						type: 'list',
+						name: 'fileName',
+						message: 'Select a file:',
+						choices: jsonFiles,
+					},
+			  ])
+			: { fileName: jsonFiles[0] }
 
-const renderOutput = ({ fileName }) => {
 	const posts = require(`../output/${fileName}`)
 
 	logEmptyLine()
 	console.log(inspect(posts.reduce((a, b) => Object.assign(a, b), {})))
-}
-
-selectFile().then(renderOutput)
+	logEmptyLine()
+})
--- src/listValues.js
+++ src/listValues.js
@@ -3,46 +3,39 @@ import { readdir } from 'fs'
 
 import { logEmptyLine } from './utils'
 
-const selectFile = () =>
-	new Promise(resolve =>
-		readdir('./output', (err, files) => {
-			const jsonFiles = files.filter(file => file.slice(-5) === '.json')
+readdir('./output', async (err, files) => {
+	const jsonFiles = files.filter(file => file.slice(-5) === '.json')
 
-			if (!jsonFiles.length) {
-				console.log('🛑  No json files found in the output directory')
-				process.exit()
-			}
+	if (!jsonFiles.length) {
+		console.log('🛑  No json files found in the output directory')
+		process.exit()
+	}
 
-			return jsonFiles.length > 1
-				? inquirer
-						.prompt([
-							{
-								type: 'list',
-								name: 'fileName',
-								message: 'Select a file:',
-								choices: jsonFiles,
-							},
-						])
-						.then(resolve)
-				: resolve({ fileName: jsonFiles[0] })
-		}),
-	)
+	const { fileName } =
+		jsonFiles.length > 1
+			? await inquirer
+					.prompt([
+						{
+							type: 'list',
+							name: 'fileName',
+							message: 'Select a file:',
+							choices: jsonFiles,
+						},
+					])
+			: { fileName: jsonFiles[0] }
 
-const selectField = ({ fileName }) => {
 	const posts = require(`../output/${fileName}`)
 	const fields = Object.keys(posts.reduce((a, b) => Object.assign(a, b), {}))
 
-	return inquirer.prompt([
+	const { field } = await inquirer.prompt([
 		{
 			type: 'list',
 			name: 'field',
 			message: 'Select a field:',
 			choices: fields,
 		},
-	]).then(res => [posts, res])
-}
+	])
 
-const renderOutput = ([posts, { field }]) => {
 	const values = posts.reduce((set, post) => set.add(post[field]), new Set())
 
 	logEmptyLine()
@@ -54,8 +47,5 @@ const renderOutput = ([posts, { field }]) => {
 	console.log(`Found ${values.size} values for the field "${field}":`)
 	logEmptyLine()
 	console.log(JSON.stringify(values, null, 2))
-}
-
-selectFile()
-	.then(selectField)
-	.then(renderOutput)
+	logEmptyLine()
+})
